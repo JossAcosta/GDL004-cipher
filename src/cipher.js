c86b1991163877bd76d11360263fc43ce8eababf
JossAcosta <josselineacostamtz@gmail.com>
2019-10-31 12:23:07 -0600

    diseño css cambio de px a %

--- src/cipher.js
+++ src/cipher.js
@@ -5,12 +5,14 @@ window.cipher ={
       for(let i=0; i<msg.length; i++){
         let ind=msg.charAt(i);
         let pos= parseInt(ABC.indexOf(ind));
-            if(pos>=0){
-          let sum = parseInt(pos+offset);
+          if(pos>=0){
+          let sum = pos+offset;
           secretMsg+=ABC.charAt(  ((sum)%26 ) ) 
           }else{secretMsg+=ind}   
            document.getElementById("resultado_encode").innerHTML =secretMsg;
-       }  
+                    
+       } 
+        
   },
   decode: (msg, offset) => {
     let secretMsg1 = "";
@@ -20,7 +22,7 @@ window.cipher ={
         let pos= parseInt(ABC.indexOf(ind));
           if(pos>=0){
           let dif = parseInt(pos-offset);
-           secretMsg1+=ABC.charAt(  ((dif)%26 ) ) 
+           secretMsg1+=ABC.charAt(  (parseInt(pos-offset)%26 ) ) 
           }else{secretMsg1+=ind}   
            document.getElementById("resultado_decode").innerHTML =secretMsg1;
         } 
