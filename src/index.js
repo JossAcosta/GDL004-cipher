d7137543ac0d03bd5f9969a4c81d8ffa23230e66
JossAcosta <josselineacostamtz@gmail.com>
2019-11-05 11:45:21 -0600

    add lowcases function

--- src/index.js
+++ src/index.js
@@ -7,21 +7,19 @@ let msgToUser=  document.getElementById("resultado_encode").value;
 let msgToUser2=  document.getElementById("resultado_decode").value;
 
 btn.addEventListener("click", () => {
-    let msgCap= text.value;
-    let msg = msgCap.toUpperCase();
+    let msg= text.value;
+   // let msg = msgCap.toUpperCase();
     let offset = (parseInt(offsetUs.value));
-    window.cipher.encode(msg,offset);
     msgToUser = window.cipher.encode(msg, offset);
-    console.log(msgToUser);
     document.getElementById("resultado_encode").innerHTML = msgToUser;
     }  );  
     
 btn2.addEventListener("click", () => {
-        let msg2Cap = text2.value;
+        let msg2 = text2.value;
         let offset = (parseInt(offsetUs.value));
-        let msg2 = msg2Cap.toUpperCase();
+       // let msg2 = msg2Cap.toUpperCase();
        msgToUser2 = window.cipher.decode(msg2,offset);
-    document.getElementById("resultado_decode").innerHTML = msgToUser2;
+      document.getElementById("resultado_decode").innerHTML = msgToUser2;
         }  );   
 
 
