7146c3dd7c8c7adf68447e9fc8a6ec9cd523d57c
JossAcosta <josselineacostamtz@gmail.com>
2019-11-07 12:08:03 -0600

    simplicar codigo eliminacion de variables

--- src/cipher.js
+++ src/cipher.js
@@ -4,16 +4,16 @@ window.cipher ={
         for (var i = 0 ; i < string.length ; i++){  
          let ind= string.charCodeAt(i);
           if (ind >= 65 & ind <= 90) { 
-           let resultEncode = ( ind - 65 + offset) % 26 + 65;
-           secretMsg +=  String.fromCharCode(resultEncode); 
+          // let resultEncode = 
+           secretMsg +=  String.fromCharCode(( ind - 65 + offset) % 26 + 65); 
            
           } if (ind >= 97 && ind <= 122){
-            let resultEncode = ( ind - 97 + offset) % 26 + 97;
-            secretMsg+=  String.fromCharCode(resultEncode); 
+            //let resultEncode = ( ind - 97 + offset) % 26 + 97;
+            secretMsg+=  String.fromCharCode(( ind - 97 + offset) % 26 + 97); 
            
           }if (ind >= 32 && ind <= 64){
-            let resultEncode = ind;
-            secretMsg+=  String.fromCharCode(resultEncode); 
+            //let resultEncode = ind;
+            secretMsg+=  String.fromCharCode(ind); 
            
           }/*if(ind === 32){ 
            secretMsg+= ' '; 
@@ -29,14 +29,14 @@ window.cipher ={
     for(let i=0; i<string.length; i++){
     let ind=string.charCodeAt(i);
     if (ind >= 65 & ind <=90){
-    let resultDecode = ( ind- 90 - offset) % 26 + 90; 
-    decodeMsg += String.fromCharCode(resultDecode); 
+    //let resultDecode = ( ind- 90 - offset) % 26 + 90; 
+    decodeMsg += String.fromCharCode(( ind- 90 - offset) % 26 + 90); 
     } if (ind >= 97 & ind <=122){
-      let resultDecode = ( ind- 122 - offset) % 26 + 122; 
-      decodeMsg += String.fromCharCode(resultDecode); 
+     // let resultDecode = ( ind- 122 - offset) % 26 + 122; 
+      decodeMsg += String.fromCharCode(( ind- 122 - offset) % 26 + 122); 
     } if(ind >= 32 & ind <=64){
-      let resultDecode = ind; 
-      decodeMsg += String.fromCharCode(resultDecode); 
+      //let resultDecode = ind; 
+      decodeMsg += String.fromCharCode(ind); 
     }/*else if (ind===32){
       decodeMsg+=' ';
     }else if (ind===209){
